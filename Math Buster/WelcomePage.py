546e203ea7f70b7a0e8f14f68d2f3fb1a4132b40
xmhonda23 <53665692+xmhonda23@users.noreply.github.com>
2020-09-19 20:16:18 -0400

    Folder for front page
    This also includes a small amount of code for the back end.

---
+++ Math Buster/WelcomePage.py
@@ -0,0 +1,12 @@
+#def contact():
+  #  if request.method == 'POST':
+  #      if request.form['submit_button'] == 'Do Something':
+  #          pass print("it works 1")
+  #      elif request.form['submit_button'] == 'Do Something Else':
+  #          pass # do something else
+  #      else:
+  #          pass # unknown
+  #  elif request.method == 'GET':
+  #      return render_template('contact.html', form=form)
+
+  # this code doesn't work
